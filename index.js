e3a2eafa266d924a82bc190383a5377de6440ab3
Toheed <your-email@example.com>
2026-07-03 02:03:47 +0500

    Added Get All Users API

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
-const User = require("./models/user.js");
+const User = require("./models/user");
 const app = express();
 
 // Middleware
